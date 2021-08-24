018a0e903aef47d4114cbb508db9dd493ed13588
vmchaz <vmchaz@gmail.com>
2021-08-25 02:25:48 +0300

    Update asyncworkers.py

--- asyncworkers.py
+++ asyncworkers.py
@@ -3,6 +3,7 @@ import asyncio
 from io import BytesIO
 import time
 import concurrent
+from logger import logging
 
 class DownloadTask:
     DEFAULT = 0
@@ -32,7 +33,7 @@ class TaskList:
         self.failed_tasks = {}
         
     def get_task(self):
-        print(len(self.tasks))
+        logging.info("Files remains:", len(self.tasks))
         return self.tasks.pop()        
         
     def add_task(self, task):
@@ -44,53 +45,49 @@ class TaskList:
         
 
 async def async_worker(tl, handler_proc, handler_arg, worker_num):
-    print(f"Worker #{worker_num} started working")
+    logging.info(f"Worker #{worker_num} started working")
     while not tl.is_empty():
         dt = tl.get_task()
         dt.decrease_retry_count()
         address = dt.address
         ltime = time.time()
-        print(f"Worker #{worker_num} started downloading {address} at time {ltime}")
+        logging.info(f"Worker #{worker_num} started downloading {address} at time {ltime}")
         
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get(address) as response:
 
-                    print("Status:", response.status)
-                    print("Content-type:", response.headers['content-type'])
+                    logging.info("Status:", response.status)
+                    logging.info("Content-type:", response.headers['content-type'])
 
                     http_response = await response.read()
-                    #tl.finished_tasks[address] = http_response
-                    print("Finished downloading ", address)                   
+
+                    dt.update_status(DownloadTask.SUCCESS)
+                    logging.info("Finished downloading ", address)
                     
                     local_name = address.split("/")[-1]
-                    
-                    if local_name == "makaba.css":
-                        local_name = "mk.css"
-                        
-                    ldict = {"data": http_response, "address": address, "local_name": local_name, "worker":worker_num}
-                    handler_proc(ldict, handler_arg)
-                    
-                    #print("Body:", html[:100], "...")
+
+                    handler_proc(address, local_name, http_response, worker_num, handler_arg)
+
         except (aiohttp.client_exceptions.ClientOSError, aiohttp.client_exceptions.ServerDisconnectedError, 
             concurrent.futures._base.TimeoutError, aiohttp.client_exceptions.ClientPayloadError) as E:
-            print("Exception while downloading", address)
-            print("Exception type", E)
-            print("Adding task to the end of the queue... (retries:", dt.retry_count, ")")
+            logging.error("Exception while downloading", address)
+            logging.error("Exception type", E)
+            logging.error("Adding task to the end of the queue... (retries:", dt.retry_count, ")")
             
             if dt.retry_count > 0:
                 tl.add_task(dt)
             
         except aiohttp.client_exceptions.InvalidURL as E:
-            print("Exception while downloading", address)
-            print("Exception type", E)
+            logging.error("Exception while downloading", address)
+            logging.error("Exception type", E)
             tl.failed_tasks[address] = "" 
         
         ltime = time.time()                
-        print(f"Worker #{worker_num} finished downloading {address} at time {ltime}")
-    print(f"Worker #{worker_num} finished working")
+        logging.info(f"Worker #{worker_num} finished downloading {address} at time {ltime}")
+    logging.info(f"Worker #{worker_num} finished working")
     
-def run_download_loop(workers_count, handler_proc, handler_tag):
+def run_download_loop(workers_count, tasklist, handler_proc, handler_tag):
     loop = asyncio.get_event_loop()
     tasks = [async_worker(tasklist, handler_proc, handler_tag, i) for i in range(workers_count)]
     wait_tasks = asyncio.wait(tasks)
